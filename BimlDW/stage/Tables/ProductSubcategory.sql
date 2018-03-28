c4a6c8721cd67195d1c73584669f04f8c3238fd5
rajjeet <rajjeet.phull@gmail.com>
2018-03-28 07:32:56 -0400

    Added package for creating staging schema based on schema & table selection

---
+++ BimlDW/stage/Tables/ProductSubcategory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stage].[ProductSubcategory] (
+    [ProductSubcategoryID] INT              NOT NULL,
+    [ProductCategoryID]    INT              NOT NULL,
+    [Name]                 NVARCHAR (50)    NOT NULL,
+    [rowguid]              UNIQUEIDENTIFIER NOT NULL,
+    [ModifiedDate]         DATETIME         NOT NULL
+);
+
